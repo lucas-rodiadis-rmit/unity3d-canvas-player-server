aeebedb55a38a5d474824b64b2c1fbadf3f2a43d
lucas-rodiadis-rmit <s3935364@student.rmit.edu.au>
2025-05-09 08:21:24 +1000

    Untested router push

--- src/routes/unityRouter.ts
+++ src/routes/unityRouter.ts
@@ -7,21 +7,37 @@ const router = Router();
 
 // Serve Unity player at the 'unity-player' endpoint
 router.post(
-	"/unity-player",
+	"/unity-player/:appId",
 	function (req: Request, res: Response) {
 		// res.send("<html> <iframe src='../../dist/index.html' width='50%' 'height=50%' /> </html>");
 
+		if (!req.params.appId) {
+			res.status(400).send("Bad request.");
+			return;
+		}
+		// Stub for clinic sim
+		else if (req.params.appId != "test123456") {
+			res.status(404).send(
+				`No app found for ${req.params.appId}.`
+			);
+			return;
+		}
+
+		// TODO: Make this fetch from the database instead
+		const appConfig = {
+			buildUrl: "/ClinicSim/Build"
+		};
+
+		// TODO: Log that someone created a session for the app
 		let reactEntryPoint: string = fs.readFileSync(
-			path.join(__dirname, "../../dist/index.html"),
+			path.join(process.cwd(), "dist", "index.html"),
 			"utf-8"
 		);
 
 		// TODO: Fix this to be robust against XSS scripting
 		reactEntryPoint = reactEntryPoint.replace(
 			"__UNITY_CONFIG__",
-			JSON.stringify({
-				buildUrl: "/ClinicSim/Build"
-			})
+			JSON.stringify(appConfig)
 		);
 
 		res.set("Content-Type", "text/html");
