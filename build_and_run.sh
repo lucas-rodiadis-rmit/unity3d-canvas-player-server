3862d96406775eab698ee1ebc8c4052822fc70c3
Natchanon Laoharawee <s4006840@student.rmit.edu.au>
2025-05-11 23:22:16 +1000

    WIP(fixDirectories): began changing build script to separate files

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-killall node
+# Kill existing node on the port to avoid port conflicts
+kill $(lsof -ti tcp:$PORT)
+# kill -9 $(lsof -ti tcp:$PORT)
+# Force kill, if necessary
 
-mkdir -p ./resources/player &&
-cd ../client/ &&
-npx vite build && cp -ra ./dist/* ../server/resources/player &&
+# Get path variables from a .env file
+# The following should be defined in .env as they are used in this script:
+#   PORT, 
+#   UNITY_PROJECTS_DIR
+source .env
+
+# Create the folder to store the unity projects, if it doesn't already exist
+mkdir -p "$UNITY_PROJECTS_DIR" &&
+
+
+# Build client-side code
+../client/build_client_side.sh &&
+
+
+# Move to server repository
 cd ../server &&
+# (Assuming dependencies are installed) Build the server and run it
 npm run build && npm start
