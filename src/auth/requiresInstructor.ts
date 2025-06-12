51e871980f8d1f3adea73276bdf15f4c15f7df6d
lucas-rodiadis-rmit <s3935364@student.rmit.edu.au>
2025-06-12 12:02:17 +1000

    Updated requires instructor

--- src/auth/requiresInstructor.ts
+++ src/auth/requiresInstructor.ts
@@ -1,21 +1,25 @@
 import { NextFunction, Request, Response } from "express";
+import requiresCanvasUser from "./requiresCanvasUser";
 
 const requiresInstructor = (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
-	if (
-		!req.session.user ||
-		!req.session.user.isInstructor
-	) {
-		res.status(403).send(
-			"This route is for instructors only."
-		);
-		return;
-	}
+	// Call requiresCanvasUser, then call our logic afterwards
+	requiresCanvasUser(req, res, (err?: any) => {
+		if (
+			!req.session.user ||
+			!req.session.user.isInstructor
+		) {
+			res.status(403).send(
+				"This route is for instructors only."
+			);
+			return;
+		}
 
-	next();
+		next();
+	});
 };
 
 export default requiresInstructor;
